d14612cf12cf694e27562c919c5201c53f86c0b8
P4Peemo <eggtheclairvoyant@126.com>
2022-05-06 18:42:37 +0800

    Q11

---
+++ 11.盛最多水的容器.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode.cn id=11 lang=python3
+#
+# [11] 盛最多水的容器
+#
+
+# @lc code=start
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l, r = 0, len(height) - 1
+        max_area = 0
+
+        while l < r:
+            curr_area = min(height[l], height[r]) * (r - l)
+            max_area = max(max_area, curr_area)
+
+            if height[l] <= height[r]:
+                l += 1
+            else:
+                r -= 1
+        return max_area
+
+# @lc code=end
+
